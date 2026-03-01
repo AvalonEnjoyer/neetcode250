c5fc1081730090f9993c1deccbd5b5f31ec95a32
AvalonEnjoyer <npathak76@gmail.com>
2026-03-01 23:08:42 +1100

    Complete 1 concatenation of array
    Signed-off-by: AvalonEnjoyer <npathak76@gmail.com>

---
+++ 1_concatenation_of_array.py
@@ -0,0 +1,8 @@
+from typing import List
+# Solution 1 - 100% runtime and 94.81% memor
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums+nums
+    # Time complexity: O(n)
+    # Space complexity: O(n)
+
