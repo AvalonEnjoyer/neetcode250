d50cf7ef6dffc42a61058e3300deeff5e743af71
AvalonEnjoyer <npathak76@gmail.com>
2026-03-04 12:09:45 +1100

    Proper implementation of buckets for 9 design hashset
    Signed-off-by: AvalonEnjoyer <npathak76@gmail.com>

--- 9_design_hashset.py
+++ 9_design_hashset.py
@@ -1,25 +1,37 @@
+# Solution 1- 100% runtime and 91.29% memory
+
 class MyHashSet:
-    def __init__(self):
-        self.hashset = set()
+    def __init__(self, size=1000):
+        self.size = size
+        self.buckets = [[] for _ in range(size)]
+
+    def _hash(self, key: int) -> int:
+        return key % self.size
 
     def add(self, key: int) -> None:
-        self.hashset.add(key)
+        bucket = self.buckets[self._hash(key)]
+        if key not in bucket:
+            bucket.append(key)
 
     def remove(self, key: int) -> None:
-        if self.contains(key):
-            self.hashset.remove(key)
+        bucket = self.buckets[self._hash(key)]
+        if key in bucket:
+            bucket.remove(key)
 
     def contains(self, key: int) -> bool:
-        return True if key in self.hashset else False
-
-
+        bucket = self.buckets[self._hash(key)]
+        return True if key in bucket else False
 
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+# obj.add(1)
+# obj.remove(0)
+# param_3 = obj.contains(0)
+# print(param_3)
 
 # ["MyHashSet","add","add","contains","contains","add","contains","remove","contains"]
 # [[],[1],[2],[1],[3],[2],[2],[2],[2]]
 # expected [null,null,null,true,false,null,true,null,false]
+
+# set = MyHashSet()
+# print(set)
