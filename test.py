315c1495518d3b2504ab27c4525db8e2afe5a031
AvalonEnjoyer <npathak76@gmail.com>
2026-03-08 00:51:22 +1100

    Update test.py
    Signed-off-by: AvalonEnjoyer <npathak76@gmail.com>

--- test.py
+++ test.py
@@ -1,26 +1,3 @@
 # Empty file to test and delete items over time
 from typing import List
 
-
-def subarraySum(nums: List[int], k: int) -> int:
-    prefix_sum = {0: 1}  # one prefix with sum 0
-    res = 0
-    running_sum = 0
-    for num in nums:
-        running_sum += num
-        diff = running_sum - k
-        if diff in prefix_sum:
-            res += prefix_sum[diff]
-        prefix_sum[running_sum] = prefix_sum.get(running_sum, 0) + 1
-    return res
-
-nums = [2,-1,1,2]
-k = 2
-# expected = 4
-# nums = [1,1,1]
-# k = 2
-# expected = 2
-# nums = [4,4,4,4,4,4]
-# k = 4
-expected = 6
-print(subarraySum(nums, k))
